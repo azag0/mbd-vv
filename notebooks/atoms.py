65af179791a8a8dfc294050e41ce46b525f4d34e
Jan Hermann <dev@janhermann.cz>
2018-02-08 16:52:58 +0100

    update

--- notebooks/atoms.py
+++ notebooks/atoms.py
@@ -1,17 +1,17 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[13]:
 
 
 from mbdvv import app, get_solids, get_s22_set, get_s66_set, kcal, ev
-from mbdvv.mbd import MBDException, mbd_rsscs
-from pymbd import MBDCalc, from_volumes, ang, vdw_params
+from pymbd import MBDCalc, from_volumes, ang, vdw_params, get_kgrid
 
 from scipy.special import erf
 import numpy as np
 import pandas as pd
 import os
+from collections import OrderedDict
 from itertools import product, islice
 from functools import partial
 from pkg_resources import resource_stream
@@ -23,7 +23,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'svg'")
 
 
-# In[2]:
+# In[4]:
 
 
 def last(obj):
@@ -44,44 +44,73 @@ def chunks(iterable, n):
         if not chunk:
             break
         yield chunk
+        
+class Key:
+    __slots__ = ['_tpl']
+    
+    def __init__(self, *tpl):
+        self._tpl = tpl[0] if len(tpl) == 1 and isinstance(tpl, tuple) else tpl
+    
+    def __hash__(self):
+        return hash(self._tpl)
+    
+    def __getitem__(self, key):
+        return self._tpl[key]
+    
+    def __repr__(self):
+        return 'Key' + repr(self._tpl)
+    
+    def __str__(self):
+        return str(self._tpl)
+    
+    def __lt__(self, other):
+        return self._tpl < other
+    
+    def __gt__(self, other):
+        return self._tpl > other  
+    
+    def __eq__(self, other):
+        return self._tpl == other
 
 
-# In[3]:
+# In[17]:
 
 
 def ene_int(x, ds, get_key):
     key = get_key(x)
-    enes = dict(x[['fragment', 'ene']].itertuples(index=False))
+    enes = x.reset_index('key', drop=True).ene.unstack('fragment')
     cluster = ds.clusters[key]
-    return pd.Series({'ene': cluster.get_int_ene(enes)})
+    enes_int = cluster.get_int_ene(enes)
+    return enes_int
 
 def ref_delta(x, ds, get_key):
     ene = x.iloc[0]
     ref = ds.clusters[get_key(x)].energies['ref']
     delta = ene-ref
     reldelta = delta/abs(ref)
-    return pd.Series({
+    return pd.Series(OrderedDict({
         'ene': ene,
         'delta': ene-ref,
         'reldelta': (ene-ref)/abs(ref),
-    })
+    }))
 
 def ene_dft_vdw(x):
-    return pd.Series({
-        'PBE': x['PBE'],
-        'PBE+MBD': x['PBE']+x['MBD'],
-        'PBE+MBD(RPA)': x['PBE']+x['MBD(RPA)'],
-    })
+    ipbe = x.index == 'PBE'
+    x = x.where(ipbe, lambda y: y + x['PBE'])
+    x.index = x.index.where(ipbe, 'PBE+' + x.index)
+    return x
     
 def ds_stat(x):
-    return pd.Series({
+    return pd.Series(OrderedDict({
         'N': len(x.dropna()),
-        'STD': x['reldelta'].std(),
-        'mean': x['reldelta'].mean(),
+        'MRE': x['reldelta'].mean(),
         'MARE': abs(x['reldelta']).mean(),
-        'median': x['reldelta'].median(),
+        'MdRE': x['reldelta'].median(),
+        'MdARE': abs(x['reldelta']).median(),
+        'SDRE': x['reldelta'].std(),
+        'ME': x['delta'].mean(),
         'MAE': abs(x['delta']).mean(),
-    })
+    }))
 
 def splice_key(df, indexes):
     return df.reset_index().assign(
@@ -90,27 +119,130 @@ def splice_key(df, indexes):
     ).drop('key', 1).set_index(['label', 'scale', *indexes])
 
 
-# In[4]:
+# In[6]:
 
 
-def mbd_from_data(calc, data, beta, **kwargs):
+class MBDException(Exception):
+    pass
+
+
+class NegativeEigs(MBDException):
+    pass
+
+
+class NegativeAlpha(MBDException):
+    pass
+
+
+def scaled_eigs(x):
+    return np.where(x >= 0, x, -erf(np.sqrt(np.pi)/2*x**4)**(1/4))
+
+
+def mbd_rsscs(mbd_calc, coords, alpha_0, C6, R_vdw, beta, lattice=None,
+              k_grid=None, rpa=False, noscs=False, scale_eigs=True):
+    def _array(obj, *args, **kwargs):
+        if obj is not None:
+            return np.array(obj, *args, **kwargs)
+
+    coords = _array(coords, dtype=float, order='F')
+    alpha_0 = _array(alpha_0, dtype=float)
+    C6 = _array(C6, dtype=float)
+    R_vdw = _array(R_vdw, dtype=float)
+    freq, freq_w = mbd_calc.omega_grid
+    omega = 4./3*C6/alpha_0**2
+    if noscs:
+        alpha_0_rsscs = alpha_0
+        C6_rsscs = C6
+        R_vdw_rsscs = R_vdw
+        omega_rsscs = omega
+    else:
+        alpha_dyn = alpha_0/(1+(freq[:, None]/omega)**2)
+        alpha_dyn_rsscs = np.empty_like(alpha_dyn)
+        for a, a_scr in zip(alpha_dyn, alpha_dyn_rsscs):
+            sigma = (np.sqrt(2./np.pi)*a/3)**(1./3)
+            a_nlc = np.linalg.inv(
+                np.diag(np.repeat(1./a, 3)) + mbd_calc.dipole_matrix(
+                    coords, 'fermi,dip,gg', sigma=sigma, R_vdw=R_vdw,
+                    beta=beta, lattice=lattice,
+                )
+            )
+            a_scr[:] = np.array([a_nlc[i::3, i::3].sum(1) for i in range(3)]).sum(0)/3
+        alpha_0_rsscs = alpha_dyn_rsscs[0, :]
+        if np.any(alpha_0_rsscs <= 0):
+            raise NegativeAlpha(alpha_0_rsscs)
+        C6_rsscs = 3./np.pi*np.sum(freq_w[:, None]*alpha_dyn_rsscs**2, 0)
+        R_vdw_rsscs = R_vdw*(alpha_0_rsscs/alpha_0)**(1./3)
+        omega_rsscs = 4./3*C6_rsscs/alpha_0_rsscs**2
+    pre = np.repeat(omega_rsscs*np.sqrt(alpha_0_rsscs), 3)
+    if lattice is None:
+        k_grid = [None]
+    else:
+        assert k_grid is not None
+        k_grid = get_kgrid(lattice, k_grid)
+    ene = 0
+    for k_point in k_grid:
+        T = mbd_calc.dipole_matrix(
+            coords, 'fermi,dip', R_vdw=R_vdw_rsscs, beta=beta,
+            lattice=lattice, k_point=k_point
+        )
+        if rpa:
+            for u, uw in zip(freq[1:], freq_w[1:]):
+                A = np.diag(np.repeat(alpha_0_rsscs/(1+(u/omega_rsscs)**2), 3))
+                eigs = np.linalg.eigvals(A@T)
+                eigs = np.real(eigs)
+                if scale_eigs:
+                    eigs = scaled_eigs(eigs)
+                if np.any(eigs <= -1):
+                    raise NegativeEigs(k_point, u, eigs)
+                if not scale_eigs:
+                    log_eigs = np.log(1+eigs)
+                else:
+                    log_eigs = np.log(1+eigs)-eigs
+                ene += 1/(2*np.pi)*np.sum(log_eigs)*uw
+        else:
+            eigs = np.linalg.eigvalsh(
+                np.diag(np.repeat(omega_rsscs**2, 3))+np.outer(pre, pre)*T
+            )
+            if np.any(eigs < 0):
+                raise NegativeEigs(k_point, eigs)
+            ene += np.sum(np.sqrt(eigs))/2-3*np.sum(omega_rsscs)/2
+    ene /= len(k_grid)
+    return ene
+
+
+# In[7]:
+
+
+def mbd_from_data(calc, data, beta, vv_scale=None, vv_pol=False,
+                  vv_corr=True, vdw17=False, **kwargs):
     coords = data['coords']['value'].T
     species = listify(data['elems']['atom'])
     lattice = data['lattice_vector']['value'] if 'lattice_vector' in data else None
     volumes = last(data['volumes'])
-    alpha_vv = last(data['vv_pols'])
+    alpha_vv = last(data['vv_pols']).copy()
     free_atoms = last(data['free_atoms'])
     species_idx = free_atoms['species']-1
     volumes_free = free_atoms['volumes'][species_idx]
     alpha_vv_free = free_atoms['vv_pols'][:, species_idx]
     freq_w = last(data['omega_grid_w'])
-
-    alpha_0, C6, R_vdw = from_volumes(species, volumes/volumes_free)
-    alpha_0_free = np.array([vdw_params.get(sp)['alpha_0'] for sp in species])
     C6_vv = 3/np.pi*np.sum(freq_w[:, None]*alpha_vv**2, 0)
     C6_vv_free = 3/np.pi*np.sum(freq_w[:, None]*alpha_vv_free**2, 0)
-    R_vdw_1 = 2.5*alpha_0**(1/7)
-    R_vdw_vv = 2.5*alpha_vv[0]**(1/7)
+    alpha_0_free = np.array([vdw_params.get(sp)['alpha_0'] for sp in species])
+    C6_free = np.array([vdw_params.get(sp)['C6'] for sp in species])
+
+    if not vv_scale:
+        volume_scale = volumes/volumes_free
+    else:
+        volume_scale = (alpha_vv[0]/alpha_vv_free[0])**(1/vv_scale)
+    alpha_0, C6, R_vdw = from_volumes(species, volume_scale)
+    if vv_pol:
+        alpha_0 = alpha_vv[0]
+        C6 = C6_vv
+        if vv_corr:
+            alpha_0 *= alpha_0_free/alpha_vv_free[0]
+            C6 *= C6_free/C6_vv_free
+    if vdw17:
+        R_vdw = 2.5*alpha_0**(1/7)
     return mbd_rsscs(
         calc,
         coords,
@@ -121,35 +253,36 @@ def mbd_from_data(calc, data, beta, **kwargs):
     )
 
 
-# In[5]:
+# In[8]:
 
 
-def all_mbd_variants(calc, data):
-    variants = {
-        'MBD': {},
-        'MBD(RPA)': {'rpa': True},
-    }
+def all_mbd_variants(calc, data, variants):
     k_grid = np.repeat(4, 3) if 'lattice_vector' in data else None
     enes = {}
     for label, kwargs in variants.items():
+        kwargs = kwargs.copy()
+        beta = kwargs.pop('beta', 0.83)
+        throw = kwargs.pop('throw', False)
         try:
-            ene = mbd_from_data(calc, data, 0.83, k_grid=k_grid, **kwargs)
+            ene = mbd_from_data(calc, data, beta, k_grid=k_grid, **kwargs)
         except MBDException as e:
+            if throw:
+                raise e
             ene = np.nan
         enes[label] = ene
     return enes
 
 
-# In[6]:
+# In[9]:
 
 
-def calculate_solids():
+def calculate_solids(variants):
     df_dft, ds = get_solids(app.ctx)
     atom_enes = df_dft['atoms'].unstack().min(1).to_frame('ene').reset_index()[['atom', 'ene']].set_index('atom').ene
     df = []
     with MBDCalc() as mbd_calc:
         for (_, label, scale), data in tqdm(list(df_dft['solids'].loc(0)['solids', :, 1.].itertuples())):
-            key = label, scale
+            key = Key(label, scale)
             atoms = [
                 ''.join(c) for c in
                 chunks(re.split(r'([A-Z])', label)[1:], 2)
@@ -158,71 +291,115 @@ def calculate_solids():
             df.append((key, 'solid', 'PBE', pbe_ene))
             for atom in atoms:
                 df.append((key, atom, 'PBE', atom_enes[atom]))
-            enes = all_mbd_variants(mbd_calc, data)
+            try:
+                enes = all_mbd_variants(mbd_calc, data, variants)
+            except MBDException as e:
+                print(label, scale, repr(e))
+                continue
             for mbd_label, ene in enes.items():
                 df.append((key, 'solid', mbd_label, ene))
     df = pd.DataFrame(df, columns='key fragment method ene'.split())         .set_index('key fragment method'.split())
     return df, ds
 
-dataframe, dataset = calculate_solids()
+def analyse_solids(df, ds):
+    return (
+        df.loc(0)[:, :, 'PBE']
+        .groupby('key').apply(ene_int, dataset, lambda x: x.iloc[0].name[0]).stack().to_frame('ene')
+        .pipe(lambda df: pd.concat((
+            df,
+            df.xs('PBE', level='method').join(
+                dataframe.query('method != "PBE"')
+                .xs('solid', level='fragment')
+                .reset_index('method'),
+                lsuffix='_pbe', rsuffix='_vdw'
+            ).apply(lambda x: pd.Series({
+                'ene': x.ene_pbe+x.ene_vdw,
+                'method': 'PBE+' + str(x.method)
+            }), 1).set_index('method', append=True),
+        )).sort_index())
+        .assign(ene=lambda x: x.ene*ev)
+        .apply(ref_delta, axis=1, args=(dataset, lambda x: x.name[0]))
+        .pipe(splice_key, ['method'])
+        .groupby('method').apply(ds_stat)
+    )
 
 
-# In[7]:
+# In[18]:
 
 
-(
-    dataframe.loc(0)[:, :, 'PBE'].reset_index()
-    .groupby('key method'.split())
-    .apply(ene_int, dataset, lambda x: x['key'].iloc[0])
-    .pipe(lambda df: pd.concat((
-        df,
-        df.xs('PBE', level='method').join(
-            dataframe.query('method != "PBE"')
-            .xs('solid', level='fragment')
-            .reset_index('method'),
-            lsuffix='_pbe', rsuffix='_vdw'
-        ).apply(lambda x: pd.Series({
-            'ene': x.ene_pbe+x.ene_vdw,
-            'method': 'PBE+' + str(x.method)
-        }), 1).set_index('method', append=True),
-    )).sort_index())
-    .assign(ene=lambda x: x.ene*ev)
-    .apply(ref_delta, axis=1, args=(dataset, lambda x: x.name[0]))
-    .pipe(splice_key, ['method'])
-    .groupby('method').apply(ds_stat)
-)
+variants = {
+    'MBD': {},
+    'MBD(RPA)': {'rpa': True},
+    'MBD(vdw17)': {'vdw17': True},
+    'MBD(VV-scale[1])': {'vv_scale': 1},
+    'MBD(vvpol)': {'vv_pol': True},
+    'MBD(RPA,vvpol)': {'rpa': True, 'vv_pol': True},
+    'MBD(vvpol,noscs)': {'vv_pol': True, 'noscs': True},
+    'MBD(RPA,vvpol,noscs)': {'rpa': True, 'vv_pol': True, 'noscs': True},
+    'MBD(vvpol,nocorr)': {'vv_pol': True, 'vv_corr': False},
+    'MBD(RPA,vvpol,nocorr)': {'rpa': True, 'vv_pol': True, 'vv_corr': False},
+    'MBD(vvpol,nocorr,noscs)': {'vv_pol': True, 'vv_corr': False, 'noscs': True},
+    'MBD(RPA,vvpol,nocorr,noscs)': {'rpa': True, 'vv_pol': True, 'vv_corr': False, 'noscs': True},
+    'MBD(vvpol,vdw17)': {'vv_pol': True, 'vdw17': True},
+    'MBD(vvpol,nocorr,vdw17)': {'vv_pol': True, 'vv_corr': False, 'vdw17': True},
+    'MBD(RPA,vvpol,nocorr,vdw17)': {'rpa': True, 'vv_pol': True, 'vv_corr': False, 'vdw17': True},
+    'MBD(RPA,vvpol,nocorr,vdw17,noscs)': {'rpa': True, 'vv_pol': True, 'vv_corr': False, 'vdw17': True, 'noscs': True},
+}
+dataframe, dataset = calculate_solids(variants)
+analyse_solids(dataframe, dataset)
 
 
-# In[8]:
+# In[19]:
 
 
-def calculate_s66():
+def calculate_s66(variants):
     df_dft, ds = get_s66_set(app.ctx)
     df = []
-    with MBDCalc() as mbd_calc:
+    with MBDCalc(4) as mbd_calc:
         for (key, fragment), data in tqdm(list(df_dft.loc(0)[ds.name].itertuples())):
+            key = Key(key)
             pbe_ene = data['energy'][0]['value'][0]
             df.append((key, fragment, 'PBE', pbe_ene))
-            enes = all_mbd_variants(mbd_calc, data)
+            enes = all_mbd_variants(mbd_calc, data, variants)
             for mbd_label, ene in enes.items():
                 df.append((key, fragment, mbd_label, ene))
     df = pd.DataFrame(df, columns='key fragment method ene'.split())         .set_index('key fragment method'.split())
     return df, ds
 
-dataframe, dataset = calculate_s66()
+def analyse_s66(df, ds):
+    df = (
+        df
+        .groupby('key').apply(ene_int, ds, lambda x: x.iloc[0].name[0])
+        .apply(ene_dft_vdw, 1).rename_axis('method', 1).stack().to_frame('ene')
+        .assign(ene=lambda x: x.ene*kcal)
+        .apply(ref_delta, 1, args=(ds, lambda x: x.name[0]))
+        .pipe(splice_key, ['method'])
+    )
+    return pd.concat((
+        df.loc(0)[:, 1.].groupby('method scale'.split()).apply(ds_stat),
+        df.loc(0)[:, 2.].groupby('method scale'.split()).apply(ds_stat),
+        df.groupby('method').apply(ds_stat).assign(scale=np.inf).set_index('scale', append=True),
+    )).sort_index()
 
 
-# In[9]:
+# In[20]:
 
 
-(
-    dataframe.reset_index()
-    .groupby('key method'.split())
-    .apply(ene_int, dataset, lambda x: x['key'].iloc[0])
-    ['ene'].unstack().apply(ene_dft_vdw, 1).rename_axis('method', 1).stack().to_frame('ene')
-    .assign(ene=lambda x: x.ene*kcal)
-    .apply(ref_delta, 1, args=(dataset, lambda x: x.name[0]))
-    .pipe(splice_key, ['method'])
-    .groupby('method').apply(ds_stat)
-)
+variants = {
+    'MBD': {},
+    'MBD(RPA)': {'rpa': True},
+    'MBD(VV-scale[1])': {'vv_scale': 1},
+    'MBD(VV-scale[1.33])': {'vv_scale': 1.33},
+    'MBD(vdw17)': {'vdw17': True},
+    'MBD(vdw17,beta=0.8)': {'vdw17': True, 'beta': 0.8},
+    'MBD(vdw17,beta=0.86)': {'vdw17': True, 'beta': 0.86},
+    'MBD(vvpol)': {'vv_pol': True},
+    'MBD(vvpol,noscs)': {'vv_pol': True, 'noscs': True},
+    'MBD(vvpol,nocorr)': {'vv_pol': True, 'vv_corr': False},
+    'MBD(vvpol,nocorr,noscs)': {'vv_pol': True, 'vv_corr': False, 'noscs': True},
+    'MBD(vvpol,noscs,vdw17)': {'vv_pol': True, 'vdw17': True, 'noscs': True},
+    'MBD(vvpol,noscs,nocorr,vdw17)': {'vv_pol': True, 'vv_corr': False, 'vdw17': True, 'noscs': True},
+}
+dataframe, dataset = calculate_s66(variants)
+analyse_s66(dataframe, dataset)
 
