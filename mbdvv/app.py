9f4666b5d686dbe081e78a0b60bcb0aad4dbb05f
Jan Hermann <dev@janhermann.cz>
2018-05-17 15:37:15 +0200

    patch the Sr tight basis

--- mbdvv/app.py
+++ mbdvv/app.py
@@ -1,3 +1,5 @@
+from pkg_resources import resource_string
+
 from caf import Caf, Cellar
 from caf.executors import DirBashExecutor, DirPythonExecutor
 from caf.Tools.aims import AimsTask
@@ -10,5 +12,14 @@ cellar = Cellar(app)
 dir_bash = DirBashExecutor(app, cellar)
 dir_python = DirPythonExecutor(app, cellar)
 aims = AimsTask(dir_bash)
+
+
+def use_old_sr_basis(task):
+    key = task['speciedir'], 'Sr'
+    if key not in aims.basis_defs:
+        aims.basis_defs[key] = resource_string(__name__, 'data/38_Sr_default').decode()
+
+
+aims.features.insert(1, use_old_sr_basis)
 
 from . import calcs  # noqa
