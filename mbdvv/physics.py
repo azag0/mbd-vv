fix reduced gradient/local gap

mbdvv/physics.py
@@ -1,22 +1,25 @@
 import numpy as np
-from scipy.special import erf
+from scipy.special import erf, expit
 
 
-def reduced_grad(n, grad):
-    return grad/(2*(3*np.pi**2)**(1/3)*n*(4/3))
+eV = 1/27.211385
+
+
+def ion_pot(n, grad):
+    return (grad/n)**2/8
 
 
 def alpha_kin(n, grad, kin):
     return (
-        (kin - grad**2/(8*n)) /
-        (3/10*(3*np.pi**2)**(2/3)*n**(5/3))
+        (kin - grad**2/(8*n))
+        / (3/10*(3*np.pi**2)**(2/3)*n**(5/3))
     )
 
 
 def beta_kin(n, grad, kin):
     return (
-        (kin - grad**2/(8*n)) /
-        (kin + 3/10*(3*np.pi**2)**(2/3)*n**(5/3))
+        (kin - grad**2/(8*n))
+        / (kin + 3/10*(3*np.pi**2)**(2/3)*n**(5/3))
     )
 
 
@@ -24,30 +27,37 @@ def terf(x, *, k, x0):
     return np.where(x > 0, 0.5*(erf(k*(x+x0))+erf(k*(x-x0))), 0)
 
 
-# rgrad_cutoff = partial(terf, k=60, x0=0.07)
+def logistic(x, *, w, x0):
+    return expit(4/w*(x-x0))
+
+
+def scanintp(x, *, c):
+    return np.where(x < 0, 1, np.where(x > 1, 0, np.exp(-c*x/(1-x))))
 
 
 def vv_pol(n, grad, C=0.0093, u=0.):
     return n/(4*np.pi/3*n+C*(grad/n)**4+u**2)
 
 
-def nm_cutoff(rgrad, alpha):
+def nm_cutoff(ion, alpha):
+    grad_normed = np.sqrt(2*ion)/((3*np.pi**2)**(1/3)*4/3)
     return 1 - (
-        (1-terf(rgrad, k=60, x0=0.12)) *  # high gradient
-        terf(alpha-10*rgrad, k=6, x0=0.7) *  # low alpha, tilted
-        (1-terf(alpha, k=1.5, x0=5))  # high alpha
+        (1 - terf(grad_normed, k=60, x0=0.12))  # high gradient
+        * terf(alpha-10*grad_normed, k=6, x0=0.7)  # low alpha, tilted
+        * (1-terf(alpha, k=1.5, x0=5))  # high alpha
     )
 
 
-def lg_cutoff(rgrad, alpha):
+def lg_cutoff(ion, alpha):
+    grad_normed = np.sqrt(2*ion)/((3*np.pi**2)**(1/3)*4/3)
     return 1 - (
-        (1-terf(rgrad, k=60, x0=0.12)) *  # high gradient
-        terf(alpha-10*rgrad, k=6, x0=0.7)  # low alpha, tilted
+        (1-terf(grad_normed, k=60, x0=0.12))  # high gradient
+        * terf(alpha-10*grad_normed, k=6, x0=0.7)  # low alpha, tilted
     )
 
 
-def lg_cutoff2(rgrad, alpha):
+def lg_cutoff2(ion, alpha):
     return 1 - (
-        (1-terf(rgrad, k=40, x0=0.18)) *  # high gradient
-        terf(alpha-10*rgrad, k=6, x0=0.7)  # low alpha, tilted
+        (1 - logistic(ion, w=1*eV, x0=5*eV))
+        * (1 - scanintp(alpha-3*np.sqrt(ion), c=0.1))
     )

mbdvv/report.py
@@ -347,8 +347,8 @@ def evaluate_mbd_specs(specs, aims_data, alpha_vv=None, free_atoms_vv=None,
             result = np.nan
         df.append((*key, fragment, spec_label, result))
     df = pd.DataFrame(df, columns='label scale fragment method ene'.split())
-    methods = df['method'].unique().tolist()
-    df['method'] = df['method'].astype(pd.api.types.CategoricalDtype(categories=methods))
+    # methods = df['method'].unique().tolist()
+    # df['method'] = df['method'].astype(pd.api.types.CategoricalDtype(categories=methods))
     df.set_index('label scale fragment method'.split(), inplace=True)
     return df
 
@@ -364,7 +364,8 @@ def ene_dft_vdw(df):
     is_pbe = df.columns == 'PBE'
     df.loc(1)[~is_pbe] = df.loc(1)[~is_pbe].apply(lambda x: x + df['PBE'], axis=0)
     renamed_methods = ['PBE+' + x if x != 'PBE' else x for x in df.columns]
-    df.columns = df.columns.set_categories(renamed_methods, rename=True)
+    df.columns = pd.Series(renamed_methods, name='method')
+    # df.columns = df.columns.set_categories(renamed_methods, rename=True)
     df = df.stack().to_frame('ene')
     return df
 
@@ -527,13 +528,13 @@ def plot_matrix_df(ax, df, **kwargs):
     return plot
 
 
-def plot_rgrad_alpha(ax, df, norm=None):
+def plot_ion_alpha(ax, df, norm=None):
     weights = df.vvpol*df.part_weight
     if norm is not None:
         weights = weights/np.sum(weights)*norm
     return ax.hist2d(
-        df.rgrad, df.alpha,
-        range=[(-.4/100, .4), (0, 12)],
+        df.ion_pot, df.alpha,
+        range=[(-1/100, 1), (0, 12)],
         bins=100,
         weights=weights,
         norm=mpl.colors.LogNorm(1e-3, 4, clip=True),

report/report.py
@@ -14,7 +14,7 @@ from scipy.interpolate import interp1d
 
 from mbdvv.app import kcal, ev
 
-from mbdvv.physics import reduced_grad, vv_pol, alpha_kin, nm_cutoff, \
+from mbdvv.physics import ion_pot, vv_pol, alpha_kin, nm_cutoff, \
     lg_cutoff, lg_cutoff2
 import mbdvv.report as rp
 
@@ -434,7 +434,7 @@ energies_s66_vvopt.groupby('method').apply(rp.dataset_scale_stats) \
 # metallic systems.  To avoid this double-counting of the electron correlation,
 # we smoothly cut off the VV polarizability functional in metallic-density
 # regions.  These regions can be distinguished using the combination of two
-# local electron-density descriptors: the reduced gradient $s$ and the
+# local electron-density descriptors: the local gap $I$ and the
 # iso-orbital indicator $\alpha$,
 # $$
 # s[n]=\frac{|\boldsymbol\nabla n|}{2(3\pi^2)^\frac13n^\frac43},\quad
@@ -470,7 +470,7 @@ s66_subset_pts = (
     .assign(kin_dens=lambda x: x.kin_dens/2)
     .assign(
         alpha=lambda x: alpha_kin(x.rho, x.rho_grad_norm, x.kin_dens),
-        rgrad=lambda x: reduced_grad(x.rho, x.rho_grad_norm),
+        ion_pot=lambda x: ion_pot(x.rho, x.rho_grad_norm),
         vvpol=lambda x: vv_pol(x.rho, x.rho_grad_norm),
     )
 )
@@ -479,7 +479,7 @@ s66_subset_pts = (
 
 # ::hide
 fig, ax = plt.subplots(figsize=(4, 3))
-*_, img = rp.plot_rgrad_alpha(ax, s66_subset_pts)
+*_, img = rp.plot_ion_alpha(ax, s66_subset_pts)
 fig.colorbar(img)
 
 # ::>
@@ -512,14 +512,14 @@ def plot_benzene(axes):
             .assign(kin_dens=lambda x: x.kin_dens/2)
             .assign(
                 alpha=lambda x: alpha_kin(x.rho, x.rho_grad_norm, x.kin_dens),
-                rgrad=lambda x: reduced_grad(x.rho, x.rho_grad_norm),
+                ion_pot=lambda x: ion_pot(x.rho, x.rho_grad_norm),
                 vvpol=lambda x: vv_pol(x.rho, x.rho_grad_norm)/nmol,
             )
         )
-        rp.plot_rgrad_alpha(ax, df)
+        rp.plot_ion_alpha(ax, df)
         ax.set_title(label)
         ax2.hist(
-            df.rgrad, range=(0, .4), bins=100, weights=df.vvpol*df.part_weight,
+            df.ion_pot, range=(0, 1), bins=100, weights=df.vvpol*df.part_weight,
             lw=0,
         )
 
@@ -542,7 +542,7 @@ plot_benzene(axes)
 # vast majority of the polarizability in main-group metals comes from the
 # regions of the electron density that is close to the uniform electron gas
 # ($s\sim0$, $\alpha\sim1$). In transition metals, the polarizability is
-# distributed over a large range of the reduced gradient along the $\alpha\sim1$
+# distributed over a large range of the local gap along the $\alpha\sim1$
 # line, with a larger part still coming from low-gradient regions. These
 # features are largely shared by the transition-metal carbides and nitrides, but
 # the whole distribution is shifted to larger values of $\alpha\sim2$.
@@ -564,7 +564,7 @@ g = sns.FacetGrid(
     .loc[lambda x: x.rho > 0]
     .assign(
         alpha=lambda x: alpha_kin(x.rho, x.rho_grad_norm, x.kin_dens),
-        rgrad=lambda x: reduced_grad(x.rho, x.rho_grad_norm),
+        ion_pot=lambda x: ion_pot(x.rho, x.rho_grad_norm),
         vvpol=lambda x: vv_pol(x.rho, x.rho_grad_norm),
     )
     .groupby('label')
@@ -577,8 +577,8 @@ g = sns.FacetGrid(
     height=2.4,
 )
 g.map_dataframe(
-    lambda x, y, data, **kwargs: rp.plot_rgrad_alpha(plt, data),
-    'alpha', 'reduced gradient'
+    lambda x, y, data, **kwargs: rp.plot_ion_alpha(plt, data, norm=60),
+    'local gap', 'alpha'
 )
 
 
@@ -609,14 +609,14 @@ g.map_dataframe(
 
 # ::hide
 def plot_cutoff(ax, cutoff, title):
-    rgrad = np.linspace(0, .4, 1000)
+    ion_pot = np.linspace(0, 1, 1000)
     alpha = np.linspace(0, 12, 1000)
-    ax.set_xlabel(r'$s$')
+    ax.set_xlabel(r'$I$')
     ax.set_ylabel(r'$\alpha$')
     ax.set_title(title)
     ax.label_outer()
     return ax.contourf(
-        rgrad, alpha, cutoff(rgrad, alpha[:, None]),
+        ion_pot, alpha, cutoff(ion_pot, alpha[:, None]),
         np.linspace(0, 1, 10)
     )
 
@@ -1289,8 +1289,8 @@ energies_s66_vdw = rp.specs_to_binding_enes([
     {'scs': True, 'beta': 0.82, 'Rvdw_vol_scale': Fraction('1/7'), 'Rvdw17_base': True},
     {'scs': True, 'beta': 0.82, 'Rvdw17': True},
     {'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.79, 'Rvdw17': True},
-    {'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.77, 'Rvdw17': True, 'vv_norm': 'nonsph'},
-    {'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.83, 'Rvdw_scale_vv': 'cutoff', 'Rvdw17_base': True, 'vv_norm': 'nonsph'},
+    {'vv': 'lg2', 'C_vv': 0.0093, 'beta': 0.77, 'Rvdw17': True, 'vv_norm': 'nonsph'},
+    {'vv': 'lg2', 'C_vv': 0.0093, 'beta': 0.81, 'Rvdw_scale_vv': 'cutoff', 'Rvdw17_base': True, 'vv_norm': 'nonsph'},
 ], s66_ds, aims_data_s66, alpha_vvs_s66, free_atoms_vv, unit=kcal)
 
 
@@ -1418,9 +1418,9 @@ energies_solids_vdw = rp.specs_to_binding_enes(
     [
         {},
         {'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.79, 'Rvdw17': True},  # optimal beta
-        {'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.77, 'Rvdw17': True, 'vv_norm': 'aims'},  # optimal beta
-        {'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.77, 'Rvdw17': True, 'vv_norm': 'aims', 'vdw_ref': 'BG'},
-        {'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.83, 'Rvdw_scale_vv': 'cutoff', 'Rvdw17_base': True, 'vv_norm': 'aims'},
+        {'vv': 'lg2', 'C_vv': 0.0093, 'beta': 0.77, 'Rvdw17': True, 'vv_norm': 'aims'},  # optimal beta
+        {'vv': 'lg2', 'C_vv': 0.0093, 'beta': 0.77, 'Rvdw17': True, 'vv_norm': 'aims', 'vdw_ref': 'BG'},
+        {'vv': 'lg2', 'C_vv': 0.0093, 'beta': 0.83, 'Rvdw_scale_vv': 'cutoff', 'Rvdw17_base': True, 'vv_norm': 'aims'},
     ],
     solids_ds,
     aims_data_solids.loc(0)[:, 1.],
@@ -1621,15 +1621,15 @@ def tmp():
         {},
         {
             'vv': 'lg2', 'beta': 0.83, 'Rvdw_scale_vv': 'cutoff',
-            'vv_norm': 'aims', 'k_grid': (2, 2, 1), '_label': 'MBD@rlg2VV[scale]'
-        },
-        {
-            'vv': 'lg2', 'beta': 0.77, 'Rvdw17': True, 'vv_norm': 'aims',
-            'k_grid': (2, 2, 1), '_label': 'MBD@rlg2VV[1/7]'
+            'vv_norm': 'aims', 'k_grid': (2, 2, 1), '_label': 'MBD-NL'
         },
         {
             'scs': True, 'beta': 0.83, 'vdw_ref': 'surf', 'k_grid': (2, 2, 1),
             '_label': 'MBD@rsSCS[surf]'
+        },
+        {
+            'ts': True, 'param_a': 20, 'beta': 0.96, 'damping': 'fermi',
+            'vdw_ref': 'surf', '_label': 'TS[surf]'
         },
     ], aims_data)
 

scripts/figs.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 from scipy.interpolate import interp1d
 
-from mbdvv.physics import reduced_grad, vv_pol, alpha_kin
+from mbdvv.physics import ion_pot, vv_pol, alpha_kin
 from mbdvv.app import kcal, ev
 import mbdvv.report as rp
 
@@ -99,7 +99,7 @@ else:
         .loc[lambda x: x.rho > 0]
         .assign(
             alpha=lambda x: alpha_kin(x.rho, x.rho_grad_norm, x.kin_dens),
-            rgrad=lambda x: reduced_grad(x.rho, x.rho_grad_norm),
+            ion_pot=lambda x: ion_pot(x.rho, x.rho_grad_norm),
             vvpol=lambda x: vv_pol(x.rho, x.rho_grad_norm),
         )
         .groupby('label')
@@ -116,8 +116,8 @@ else:
             {},
             {'rpa': True, 'scs': True, 'beta': 0.83, '_label': 'MBD@rsSCS'},
             {
-                'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.83, 'Rvdw_scale_vv': 'cutoff',
-                'Rvdw17_base': True, 'vv_norm': 'aims', '_label': 'MBD@VV'
+                'vv': 'lg2', 'C_vv': 0.0093, 'beta': 0.81, 'Rvdw_scale_vv': 'cutoff',
+                'Rvdw17_base': True, 'vv_norm': 'aims', '_label': 'MBD-NL'
             },
         ],
         solids_ds,
@@ -133,7 +133,7 @@ fig, axes = plt.subplots(
 grouped = solids_vv.groupby('group')
 for ax, group in zip(axes.flat, solid_groups.values()):
     df = grouped.get_group(group)
-    im = rp.plot_rgrad_alpha(ax, df, norm=62)[-1]
+    im = rp.plot_ion_alpha(ax, df, norm=62)[-1]
     ax.set_title(group)
     ax.set_xticks([0, 0.3])
     ax.set_yticks([0, 1, 10])
@@ -179,11 +179,11 @@ for ax, label, nmol, row in payload:
         .assign(kin_dens=lambda x: x.kin_dens/2)
         .assign(
             alpha=lambda x: alpha_kin(x.rho, x.rho_grad_norm, x.kin_dens),
-            rgrad=lambda x: reduced_grad(x.rho, x.rho_grad_norm),
+            ion_pot=lambda x: ion_pot(x.rho, x.rho_grad_norm),
             vvpol=lambda x: vv_pol(x.rho, x.rho_grad_norm)/nmol,
         )
     )
-    rp.plot_rgrad_alpha(ax, df)
+    rp.plot_ion_alpha(ax, df)
     ax.set_title(label)
     ax.set_xticks([0, 0.3])
     ax.set_xticklabels([0, 0.3])
@@ -206,7 +206,7 @@ else:
                 {'vv': True, 'C_vv': 0.0101, 'beta': np.nan, '_label': 'VV'},
                 {'vv': 'nm', 'C_vv': 0.0101, 'beta': np.nan, '_label': 'nmVV'},
                 {'vv': 'lg', 'C_vv': 0.0101, 'beta': np.nan, '_label': 'lgVV'},
-                {'vv': 'lg2', 'C_vv': 0.0101, 'beta': np.nan, '_label': 'lg2VV'},
+                {'vv': 'lg2', 'C_vv': 0.0093, 'beta': np.nan, '_label': 'lg2VV'},
             ],
             aims_data_s66,
             alpha_vvs_s66,
@@ -257,8 +257,8 @@ else:
         {},
         {'scs': True, 'beta': 0.83, '_label': 'MBD@rsSCS'},
         {
-            'vv': 'lg2', 'C_vv': 0.0101, 'beta': 0.83, 'Rvdw_scale_vv': 'cutoff',
-            'Rvdw17_base': True, 'vv_norm': 'nonsph', '_label': 'MBD@VV'
+            'vv': 'lg2', 'C_vv': 0.0093, 'beta': 0.81, 'Rvdw_scale_vv': 'cutoff',
+            'Rvdw17_base': True, 'vv_norm': 'nonsph', '_label': 'MBD-NL'
         },
     ], s66_ds, aims_data_s66, alpha_vvs_s66, free_atoms_vv, unit=kcal)
     vdw_energies_s66.to_hdf(VDW_ENERGIES_S66_H5, 'table')
@@ -294,7 +294,7 @@ with sns.color_palette(list(reversed(sns.color_palette('coolwarm', 8)))):
         x='method',
         y='reldelta',
         hue='scale',
-        order='PBE PBE+MBD@rsSCS PBE+MBD@VV PBE+VV10'.split(),
+        order='PBE PBE+MBD@rsSCS PBE+MBD-NL PBE+VV10'.split(),
         aspect=1.6,
         height=1.8,
         margin_titles=True,
@@ -316,7 +316,7 @@ g = sns.catplot(
     x='method',
     y='reldelta',
     hue='group',
-    order='PBE PBE+MBD@rsSCS PBE+MBD@VV PBE+VV'.split(),
+    order='PBE PBE+MBD@rsSCS PBE+MBD-NL'.split(),
     aspect=2,
     height=1.5,
     margin_titles=True,